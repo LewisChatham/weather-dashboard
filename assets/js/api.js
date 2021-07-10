8894efa20eee253175fea710c777f2bfa422aa6e
LewisChatham <lewis.chatham@outlook.com>
2021-07-10 11:19:17 +0100

    Added api.js and functionality

---
+++ assets/js/api.js
@@ -0,0 +1,13 @@
+var API_KEY = "142a101a55377d96cb27c888467295e4"
+
+function generateUrl(city) {
+    var NUMBER_OF_DAYS = 5;
+
+    return `api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=${NUMBER_OF_DAYS}&appid=${API_KEY}`
+}
+
+function makeRequest(Url) {
+    return fetch(Url).then(function (res) {
+        return res.json();
+    })
+}
